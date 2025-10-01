063a2c075a51439ba0750832d948e45bf53134a0
Vitor Alves de Almeida <vitor9597@gmail.com>
2025-10-01 14:22:33 -0300

    Add files via upload

---
+++ pratice-logic-js3/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Jogo do Numero Secreto";
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = "Escolha um número entre 1 e 10"
+
+function verificarChute() {
+    console.log("to funcionando.")
+}
