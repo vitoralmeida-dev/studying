30f3f26e31fdfd0dc59cde4d7495f4c3c0449048
Vitor Alves de Almeida <vitor9597@gmail.com>
2025-10-01 11:40:26 -0300

    Add files via upload

---
+++ pratice-logic-js/app.js
@@ -0,0 +1,122 @@
+// ------------------------------------------------------------------------------
+
+// let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+// let soma = 0;
+// let contador = qtdNumeros;
+
+// while(contador > 0){
+//     let numero = parseInt(prompt('Digite o numero:'));
+//     soma += numero;
+//     contador--
+// }
+
+// let media = soma / qtdNumeros;
+
+// console.log(media);
+
+// ------------------------------------------------------------------------------
+
+// contador = 1;
+
+// while (contador <= 10){
+//     console.log(contador);
+//     contador++;
+// } 
+
+// ------------------------------------------------------------------------------
+
+// contador = 10;
+
+// while (contador >= 1){
+//     console.log(contador);
+//     contador--;
+// } 
+
+// ------------------------------------------------------------------------------
+
+// let contagemRegressiva = prompt("Você quer fazer a contagem regressiva a partir de qual número?");
+
+// while (contagemRegressiva >= 0) {
+//     console.log(contagemRegressiva);
+//     contagemRegressiva--;
+// }
+
+// ------------------------------------------------------------------------------
+
+// let numeroQueEleQuerChegar = prompt("Você quer fazer a contagem progressiva até qual número?");
+// let numero = 0
+
+// while ( numero <= numeroQueEleQuerChegar ) {
+//     console.log(numero);
+//     numero++;
+// }
+
+// alert(`O número é ${numero}`);
+
+// ------------------------------------------------------------------------------
+
+// idadeUsuario = Number(prompt("Qual é a sua idade?"));
+
+// if (idadeUsuario >= 18) {
+//     console.log("Você é maior de idade.");
+// } else if (idadeUsuario < 18) {
+//     console.log("Você é menor de idade.");
+// } else {
+//     console.log("Idade inválida.");
+// }
+
+// ------------------------------------------------------------------------------
+
+// let numero = Number(prompt("Insira um número:"));
+
+// if (numero > 0) {
+//     alert("O número é positivo.");
+// } else if (numero < 0) {
+//     alert("O número é negativo.");
+// } else {
+//     invalidoOuZero = numero == 0 ? "O número é neutro." : "O número inserido é inválido.";
+//     alert(invalidoOuZero);
+// }
+
+// ------------------------------------------------------------------------------
+
+// let numero = 0;
+
+// while (numero < 10) {
+
+//     numero++;
+//     console.log(numero);
+
+// }
+
+// ------------------------------------------------------------------------------
+
+// let nomeAluno = prompt("Insira o nome do Aluno:");
+
+// let notaAluno = Number(prompt(`Insira a nota do(a) ${nomeAluno}:`));
+
+// if (notaAluno >= 7) {
+//     console.log(`O(A) ${nomeAluno} está aprovado(a).`);
+// } else if (notaAluno < 7) {
+//     console.log(`O(A) ${nomeAluno} está reprovado(a).`);
+// } else {
+//     console.log("A nota inserida é inválida.");
+// }
+
+// ------------------------------------------------------------------------------
+
+// numeroAleatorio = Math.random();
+
+// console.log(numeroAleatorio);
+
+// ------------------------------------------------------------------------------
+
+// let numero1a10 = parseInt(Math.random() * 10 + 1 );
+
+// console.log(numero1a10);
+
+// ------------------------------------------------------------------------------
+
+let numero1a1000 = parseInt(Math.random() * 1000 + 1);
+
+console.log(numero1a1000);
---
+++ pratice-logic-js2/app.js
@@ -0,0 +1,87 @@
+// alert("Boas vindas ao nosso site!")
+
+// let nome = "Lua";
+// let idade = "25";
+// let numeroDeVendas = "50";
+// let saldoDisponivel = "1000";
+
+// let mensagemDeErro = "Erro! Preencha todos os campos."
+
+// alert(mensagemDeErro);
+
+// -----------------------------------------------------------------
+
+// let nome = prompt("Insira o seu nome:");
+// let idade = prompt("Insira a sua idade:");
+
+// if (idade >= 18){
+//     alert(`${nome} está APTO(A) para tirar a habilitação.`);
+// }else{
+//     alert(`${nome} está INAPTO(A) para tirar a habilitação.`);
+// }
+
+// -----------------------------------------------------------------
+
+// alert("Bem-vindo ao jogo do número secreto");
+// let numeroSecreto = 4;
+// console.log(numeroSecreto);
+// let chute = prompt("Escolha um número entre 1 e 10");
+// console.log(chute);
+
+// if (chute == numeroSecreto) {
+//     alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto})!`);
+//     console.log("acertou");
+// } else { 
+//     alert(`Você errou, o número secreto não é ${chute}.`);
+//     console.log("errou");
+// }
+
+// -----------------------------------------------------------------
+
+// let diaDaSemana = prompt("Qual é o dia da semana?")
+
+// if (diaDaSemana.toLowerCase() == "sábado" || diaDaSemana.toLowerCase() == "domingo" ){
+//     alert("Bom fim de semana!");
+// }else{
+//     alert("Boa semana!");
+// }
+
+// -----------------------------------------------------------------
+
+// let numero = prompt("Insira um número:")
+
+// if ( numero%2 == 0 ) {
+//     alert("O número é par.");
+// } else {
+//     alert("O número é ímpar.");
+// }
+
+// -----------------------------------------------------------------
+// while (true) {
+
+//     let pontuacao = prompt("Insira a sua pontuação na partida anterior:");
+
+//     if (pontuacao >= 100){
+//         alert("Parabéns, você classificou para a próxima fase.");
+//         break;
+//     } else {
+//         alert("Infelizmente você não conseguiu se classificar, tente novamente.");
+//         continue;
+//     }
+    
+// }
+
+// -----------------------------------------------------------------
+
+// let nomeDoCliente = prompt("Como devemos te chamar?")
+
+// let saldoDoUsuário = prompt("Insira o seu saldo atual:")
+
+// alert(`O saldo atual de ${nomeDoCliente} é de R$${saldoDoUsuário}!`)
+
+// -----------------------------------------------------------------
+
+// let nomeDoUsuario = prompt("Insira o seu nome completo:")
+
+// alert("Seja bem-vindo " + nomeDoUsuario + "!")
+
